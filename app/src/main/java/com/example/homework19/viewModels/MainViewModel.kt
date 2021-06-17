bd921f19eb2e032a4454478e3462566235159081
giorgi qemoklidze <gqemoklidze6@gmail.com>
2021-06-18 01:56:30 +0400

    Homework19/

--- app/src/main/java/com/example/homework19/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/homework19/viewModels/MainViewModel.kt
@@ -2,11 +2,13 @@ package com.example.homework19.viewModels
 
 import android.util.Log.d
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.homework19.Models.User
 import com.example.homework19.api.RetrofitService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
 
@@ -14,17 +16,23 @@ class MainViewModel : ViewModel() {
     lateinit var  user:User
 
     fun register(){
-        CoroutineScope(Dispatchers.IO).launch {
-            var registerResponse = RetrofitService.RetrofitService().registerUser(user)
-
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                var registerResponse = RetrofitService.RetrofitService().registerUser(user)
+            }
         }
+
     }
 
 
     fun logIn(){
-        CoroutineScope(Dispatchers.IO).launch {
-            var logInResponse = RetrofitService.RetrofitService().logIn(user)
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                var logInResponse = RetrofitService.RetrofitService().logIn(user)
+            }
         }
+
     }
 
 
